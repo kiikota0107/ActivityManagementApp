56b524f46166ce4ccc87ff9722f9f442d455d77c
kiikota <kiikota0107@gmail.com>
2025-11-06 14:58:25 +0900

    入力画面：タスク進行中にブラウザを閉じれるよう修正

--- Services/ActivityInputService.cs
+++ Services/ActivityInputService.cs
@@ -1,6 +1,7 @@
-﻿using Microsoft.EntityFrameworkCore;
-using ActivityManagementApp.Data;
+﻿using ActivityManagementApp.Data;
 using ActivityManagementApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 
 namespace ActivityManagementApp.Services
 {
@@ -13,10 +14,36 @@ namespace ActivityManagementApp.Services
             _context = context;
         }
 
+        public async Task<ActivityLogs?> FindProgressActivityLogsAsync()
+        {
+            ActivityLogs? progressActivity = await _context.ActivityLogs.Where(x => x.EndDateTime < x.StartDateTime).FirstOrDefaultAsync();
+            return progressActivity;
+        }
+
         public async Task InsertActivityLogsAsync(ActivityLogs newActivityLogs)
         {
             _context.ActivityLogs.Add(newActivityLogs);
             await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateProgressActivityLogsAsync(int id)
+        {
+            ActivityLogs? progressActivity = await _context.ActivityLogs.FindAsync(id);
+            if (progressActivity != null)
+            {
+                progressActivity.EndDateTime = DateTime.Now;
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task DeleteProgressActivityLogsAsync(int id)
+        {
+            ActivityLogs? progressActivity = await _context.ActivityLogs.FindAsync(id);
+            if (progressActivity != null)
+            {
+                _context.ActivityLogs.Remove(progressActivity);
+                await _context.SaveChangesAsync();
+            }
         }
     }
 }
