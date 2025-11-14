17e4752ae999017afc338d62c0593172d20ac54e
kiikota <kiikota0107@gmail.com>
2025-11-14 20:32:10 +0900

    入力画面：データ更新ボタン押下時のタスク詳細項目データバリデーション機能を追加

--- ViewModels/ActivityInputViewModel.cs
+++ ViewModels/ActivityInputViewModel.cs
@@ -4,10 +4,10 @@ namespace ActivityManagementApp.ViewModels
 {
     public class ActivityInputViewModel
     {
-        [MaxLength(10, ErrorMessage = "タスクタイトルは100文字以内で入力してください。")]
+        [MaxLength(100, ErrorMessage = "タスクタイトルは100文字以内で入力してください。")]
         public string ActivityDetailTitle { get; set; } = "";
 
-        [MaxLength(10, ErrorMessage = "タスク詳細は1000文字以内で入力してください。")]
+        [MaxLength(1000, ErrorMessage = "タスク詳細は1000文字以内で入力してください。")]
         public string ActivityDetail { get; set; } = "";
     }
 }
