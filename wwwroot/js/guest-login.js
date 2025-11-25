7db1dcb40aef9d4154ec64ac8c2dbc6e465c57d8
kiikota <kiikota0107@gmail.com>
2025-11-25 15:30:25 +0900

    #24 ゲストログインページは廃止。ログイン画面でメール/パスワードを自動でセットし、ログインするよう修正。

---
+++ wwwroot/js/guest-login.js
@@ -0,0 +1,6 @@
+﻿window.guestLogin = function (email, password) {
+    document.querySelector('input[name="Input.Email"]').value = email;
+    document.querySelector('input[name="Input.Password"]').value = password;
+
+    document.querySelector('#loginButton').click();
+}
