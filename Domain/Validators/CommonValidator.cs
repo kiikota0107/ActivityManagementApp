22a227725717336b5fddb8c5ee9d0948ca56ab8a
kiikota <kiikota0107@gmail.com>
2025-11-26 15:22:05 +0900

    #33 ゲストユーザによるデモデータの更新チェックを共通化

---
+++ Domain/Validators/CommonValidator.cs
@@ -0,0 +1,12 @@
+﻿namespace ActivityManagementApp.Domain.Validators
+{
+    public static class CommonValidator
+    {
+        public static CustomValidationResult ValidateUserIsOwner(string recordUserId, string currentUserId)
+        {
+            if (recordUserId != currentUserId)
+                return CustomValidationResult.InValid("登録時のユーザと異なるユーザによる更新はできません。");
+            return CustomValidationResult.Valid();
+        }
+    }
+}
