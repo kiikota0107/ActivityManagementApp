d7802db3b93c254a2beec07c448905c5885cbe0a
kiikota <kiikota0107@gmail.com>
2025-12-02 20:13:10 +0900

    #42 タスク詳細項目の文字数制限の緩和

--- ViewModels/ActivityInputViewModel.cs
+++ ViewModels/ActivityInputViewModel.cs
@@ -4,10 +4,10 @@ namespace ActivityManagementApp.ViewModels
 {
     public class ActivityInputViewModel
     {
-        [MaxLength(100, ErrorMessage = "タスクタイトルは100文字以内で入力してください。")]
+        [MaxLength(150, ErrorMessage = "タスクタイトルは150文字以内で入力してください。")]
         public string ActivityDetailTitle { get; set; } = "";
 
-        [MaxLength(1000, ErrorMessage = "タスク詳細は1000文字以内で入力してください。")]
+        [MaxLength(10000, ErrorMessage = "タスク詳細は10000文字以内で入力してください。")]
         public string ActivityDetail { get; set; } = "";
     }
 }
